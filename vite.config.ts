e86cb48e4fc84b2e3bd3cadc8225ed8659b10d96
lszemerics <lszemerics@gmail.com>
2026-02-26 13:29:35 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,17 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   base: '/aura-elevated/',
   server: {
     host: "::",
     port: 8080,
-    hmr: {
-      overlay: false,
-    },
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [
+    react(),
+    mode === 'development' &&
+    componentTagger(),
+  ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
